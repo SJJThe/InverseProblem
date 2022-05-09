ecec49a41aab5243228d0733c7d352875594625d
sthe <the.samuel.ad@gmail.com>
2022-05-09 11:24:38 +0200

    update tests to check gradients

--- test/tests_gradient.jl
+++ test/tests_gradient.jl
@@ -2,23 +2,31 @@
 using Zygote
 using InverseProblem
 const IP = InverseProblem
+using PyPlot
+const plt = PyPlot
+
+Regx = IP.edgepreserving(1e3, :v2)#IP.homogenedgepreserving(1e3, :v2)
 
 niter = 5
-Regx = IP.homogenedgepreserving(1e3, :v2)
-for i in 1:niter
-    x = randn(10,10)
-    g = zeros(size(x))
-    vext = max(abs(minimum(x)), maximum(x))
-    figure()
-    plt.subplot(121)
-    plt.imshow(x, cmap="seismic", aspect="auto", interpolation="none")
-
-    IP.call!(Regx, x, g)
-    g_zyg = gradient(y -> IP.call(Regx, y), x)[1]
-
-    plt.subplot(122)
-    plt.imshow(g - g_zyg, cmap="seismic", aspect="auto", interpolation="none",
-               vmin=-vext, vmax=vext)
+nlin = round(Int, niter/2)
+ncol = niter - nlin
+fig, axs = plt.subplots(nrows=nlin, ncols=ncol)
+fig.suptitle("residuals between Zygote and custom gradients, variance = 1")
+for i in 1:nlin
+    for j in 1:ncol
+        x = randn(10,10)
+        g = zeros(size(x))
+        # axs[i,j].imshow(x, cmap="seismic", aspect="auto", interpolation="none")
+        
+        IP.call!(Regx, x, g)
+        g_zyg = gradient(y -> IP.call(Regx, y), x)[1]
+        
+        res_g = g - g_zyg
+        vext = max(abs(minimum(res_g)), maximum(res_g))
+        im = axs[i,j].imshow(res_g, cmap="seismic", aspect="auto", interpolation="none",
+                             vmin=-vext, vmax=vext)
+        fig.colorbar(im, ax=axs[i,j])
+    end
 end
 
 gridx = -10:10
